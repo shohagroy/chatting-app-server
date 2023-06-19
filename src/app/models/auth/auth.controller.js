62ebb84cd98e6198c085e78315bb6951f1006bec
shohag <pkshohag240@gmail.com>
2023-06-20 03:52:46 +0600

    user create and login function code updated

--- src/app/models/auth/auth.controller.js
+++ src/app/models/auth/auth.controller.js
@@ -59,7 +59,7 @@ const getLoginUser = async (req, res, next) => {
     res.status(200).json({
       status: "success",
       message: "user get successfully!",
-      data: loginUser,
+      data: { user: loginUser },
     });
   } catch (error) {
     next(error);
